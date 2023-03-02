e2400490f66bb332a87113b7e0d9578a40af22b4
Toni Patsias <enchanted.toilet@gmail.com>
2023-03-02 16:06:33 +0000

    working through state and event handling

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,26 +1,21 @@
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
-
-return (
-  <div>
-    <p>
-      Hello {name}, you are {age} years old
-    </p>
-    <p>So you were probably born in {bornYear()}</p>
-  </div>
-)
-
-};
+import { useState } from "react";
 
 const App = () => {
-  const name = "Peter";
-  const age = 10;
+  const [counter, setCounter] = useState(0);
+
+  // const handleClick = () => {
+  //   console.log("clicked");
+  // };
+
+  // setTimeout(() => setCounter(counter + 1), 1000);
+
+  console.log("rendering...", counter);
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Miteux" age={5 + 2} />
-      <Hello name={name} age={age} /> 
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>plus</button>
+      <button onClick={() => setCounter(0)}>zero</button>
     </div>
   );
 };
