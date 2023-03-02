700d72bfc9f8a572ba80149c3d6d910acc11d8bb
Toni Patsias <enchanted.toilet@gmail.com>
2023-03-02 16:51:05 +0000

    Finished part 1C lesson

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,18 +2,26 @@ import { useState } from "react";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  console.log("rendering with counter value", counter);
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
-
-  const Display = (props) => {
-    return <div>{props.counter}</div>;
+  const increaseByOne = () => {
+    console.log("increasing, value before", counter);
+    setCounter(counter + 1);
   };
 
-  const Button = (props) => {
-    return <button onClick={props.handleClick}>{props.text}</button>;
+  const decreseByOne = () => {
+    console.log("decreasing, value before", counter);
+    setCounter(counter - 1);
   };
+
+  const setToZero = () => {
+    console.log("resetting to zero, value before", counter);
+    setCounter(0);
+  };
+
+  const Display = ({ counter }) => <div>{counter}</div>;
+
+  const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
   console.log("rendering...", counter);
 
