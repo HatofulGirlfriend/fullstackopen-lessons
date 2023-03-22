3be91acc2401077365ad8f10e670703a1b6e9ef2
Toni Patsias <enchanted.toilet@gmail.com>
2023-03-22 16:58:09 +0000

    more of lesson 3b

--- part2/src/services/notes.js
+++ part2/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
@@ -22,4 +22,6 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default { getAll, create, update }
+const exportObject = { getAll, create, update }
+
+export default exportObject
