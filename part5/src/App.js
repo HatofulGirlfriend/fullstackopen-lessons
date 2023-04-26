14f06957d7844661c170f8ae7d7993e7e09cc7d4
Toni Patsias <enchanted.toilet@gmail.com>
2023-04-26 14:18:42 +0100

    Lesson 5c

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,20 +1,20 @@
-import { useState, useEffect, useRef } from 'react'
-import Note from './components/Note'
-import Notification from './components/Notification'
-import Footer from './components/Footer'
-import LoginForm from './components/LoginForm'
-import NoteForm from './components/NoteForm'
-import Togglable from './components/Togglable'
-import noteService from './services/notes'
-import loginService from './services/login'
+import { useState, useEffect, useRef } from "react"
+import Note from "./components/Note"
+import Notification from "./components/Notification"
+import Footer from "./components/Footer"
+import LoginForm from "./components/LoginForm"
+import NoteForm from "./components/NoteForm"
+import Togglable from "./components/Togglable"
+import noteService from "./services/notes"
+import loginService from "./services/login"
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
 
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
   const [user, setUser] = useState(null)
 
   useEffect(() => {
@@ -26,7 +26,7 @@ const App = () => {
   }, [])
 
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    const loggedUserJSON = window.localStorage.getItem("loggedNoteappUser")
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
@@ -44,13 +44,13 @@ const App = () => {
       })
       noteService.setToken(user.token)
       window.localStorage.setItem(
-        'loggedNoteappUser', JSON.stringify(user)
-      ) 
+        "loggedNoteappUser", JSON.stringify(user)
+      )
       setUser(user)
-      setUsername('')
-      setPassword('')
+      setUsername("")
+      setPassword("")
     } catch (exception) {
-      setErrorMessage('wrong credentials')
+      setErrorMessage("wrong credentials")
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
@@ -71,24 +71,24 @@ const App = () => {
     : notes.filter(note => note.important)
 
   const toggleImportanceOf = id => {
-      const note = notes.find(n => n.id === id)
-      const changedNote = { ...note, important: !note.important }
-  
-      noteService
-        .update(id, changedNote).then(returnedNote => {
-          setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-        })
-        .catch(error => {
-          setErrorMessage(
-            `Note '${note.content}' was already removed from server`
-          )
-          setTimeout(() => {
-            setErrorMessage(null)
-          }, 5000)
-          setNotes(notes.filter(n => n.id !== id))
-        })
-   }
-  
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
   return (
     <div>
       <h1>Notes app</h1>
@@ -113,14 +113,14 @@ const App = () => {
           </Togglable>
         </div>
       }
- 
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? "important" : "all" }
         </button>
-      </div> 
+      </div>
       <ul>
-        {notesToShow.map(note => 
+        {notesToShow.map(note =>
           <Note
             key={note.id}
             note={note}
--- part5/src/components/NoteForm.js
+++ part5/src/components/NoteForm.js
@@ -1,32 +1,43 @@
-import { useState } from 'react'
+import { useState } from "react"
 
 const NoteForm = ({ createNote }) => {
-  const [newNote, setNewNote] = useState('')
+  const [newNote, setNewNote] = useState("")
+
+  const handleChange = (event) => {
+    setNewNote(event.target.value)
+  }
 
   const addNote = (event) => {
     event.preventDefault()
     createNote({
       content: newNote,
-      important: true,
+      important: Math.random() > 0.5,
     })
 
-    setNewNote('')
-
+    setNewNote("")
   }
 
   return (
-    <div>
+    <div className="formDiv">
       <h2>Create a new note</h2>
 
       <form onSubmit={addNote}>
         <input
           value={newNote}
-          onChange={event => setNewNote(event.target.value)}
+          onChange={handleChange}
+          placeholder="write note content here"
+        />
+        <input
+          value={catbat}
+          onChange={batcat}
         />
         <button type="submit">save</button>
       </form>
     </div>
   )
 }
+
+let catbat = 8
+let batcat = 9
 
 export default NoteForm
